e21a8436c5e7e2a42288cd32718d1f76cce06186
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-01-29 20:26:24 +0530

    Create GFG.java

---
+++ Palindrome/GFG.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            String ans = ob.is_palindrome(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String is_palindrome(int n)
+    {
+        // Code here
+        int reverse = 0, copy = n;
+        while(copy > 0) {
+            int num = copy % 10;
+            reverse = reverse * 10 + num;
+            copy /= 10;
+        }
+        
+        if(reverse == n)
+            return "Yes";
+        return "No";
+    }
+}
