4af242846488867907a84b8555f08f960dbb848a
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-03-10 12:03:13 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove all duplicates from a given string/remove-all-duplicates-from-a-given-string.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String str = br.readLine().trim();
+
+            String ans = new Solution().removeDuplicates(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    String removeDuplicates(String str) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            int idx = str.indexOf(str.charAt(i));
+            if(idx == i){
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
